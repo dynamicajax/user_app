14562804a49adba95b1f2a8e12f968af8391cca4
Ben <dynamicajax@gmail.com>
2014-01-02 14:44:30 -0600

    initialize new repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '490ab7b81cb75f2b9ab5b1ac825795fa5469f15e12e74e6f9b3d42b459857eef3e3eff1b3371ffc2c68dcf03b213587120274c0b704b563fa09bcb0e0b91f766'
